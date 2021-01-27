d0c7f032366231ee544c29c8f6908a6e6675d55a
Leonardo Luz Fernandes <leonardo.l.fernandes@gmail.com>
2021-01-27 20:20:27 -0300

    inicio de estudos sobre api fetch

---
+++ service/cliente-service.js
@@ -0,0 +1,27 @@
+const criarNovaLinha = ( nome , email ) => {
+    const novaLinha = document.createElement('tr');
+    const conteudo = 
+    `<td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+        <ul class="tabela__botoes-controle">
+            <li><button class="botao-simples botao-simples--editar">Editar</button></li>
+            <li><button class="botao-simples botao-simples--excluir">Excluir</button></li>
+        </ul>
+    </td>` ;
+    novaLinha.innerHTML(conteudo);
+    return novaLinha;
+}
+
+const tabela = document.querySelector('[data-tabela]');
+const http = new XMLHttpRequest();
+http.open("GET","http://localhost:3000/profile");
+http.send();
+
+http.onload = () => {
+    const data = JSON.parse( http.response );
+    data.forEach( elemento => {
+        const novaLinha = criarNovaLinha( elemento.nome , elemento.email );
+    });
+    tabela.appendChild(novaLinha);
+}
